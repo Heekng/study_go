8d63bf8bcd900dabda77377f34c0444972ea504f
heekng <rhkd1769@gmail.com>
2022-08-27 23:25:08 +0900

    var, const

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	//상수
+	//const name string = "heekng"
+	//fmt.Println(name)
+
+	//var name string = "heekng"
+	// 이렇게 작성하면 var, type을 자동 설정해준다.
+	// 축약형은 func 안에서만 사용 가능
+	name := "heekng"
+	name = "heekng2"
+	fmt.Println(name)
 }
