a12fe2312f8fd8b03f53b51a322e80059ee983be
heekng <rhkd1769@gmail.com>
2022-08-28 16:53:11 +0900

    make URLChecker without Channels

--- main.go
+++ main.go
@@ -1,56 +1,49 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/heekng/study_go/dict"
+	"net/http"
 )
 
-func main() {
-	dictionary := dict.Dictionary{"first": "First word"}
-	//dictionary["hello"] = "hello"
-	//fmt.Println(dictionary)
-	//fmt.Println(dictionary["first"])
-	/*
-		definition, err := dictionary.Search("first")
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(definition)
-		}
-	*/
-	/*
-		word := "hello"
-		definition = "Greeting"
-		err = dictionary.Add(word, definition)
-		if err != nil {
-			fmt.Println(err)
-		}
-		hello, _ := dictionary.Search(word)
-		fmt.Println("found", word, "definition:", hello)
-		err2 := dictionary.Add(word, definition)
-		if err2 != nil {
-			fmt.Println(err2)
-		}
-	*/
-	/*
-		baseWord := "hello"
-		dictionary.Add(baseWord, "First")
-		err := dictionary.Update(baseWord, "Second")
-		if err != nil {
-			fmt.Println(err)
-		}
-		word, _ := dictionary.Search(baseWord)
-		fmt.Println(word)
-	*/
+var errRequestFailed = errors.New("Request failed")
 
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(word)
+func main() {
+	var results = make(map[string]string)
+
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
+
+	for _, url := range urls {
+		//fmt.Println(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
 }
