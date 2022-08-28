4952ddebf4429efaaec0204c1fa41d32476317d5
heekng <rhkd1769@gmail.com>
2022-08-28 16:10:56 +0900

    dictionary 예제

--- main.go
+++ main.go
@@ -2,23 +2,55 @@ package main
 
 import (
 	"fmt"
-	"github.com/heekng/study_go/accounts"
+	"github.com/heekng/study_go/dict"
 )
 
 func main() {
-	//account := accounts.Account{Owner: "heekng"}
-	//fmt.Println(account)
-	//account.Owner = "pepe"
-	//fmt.Println(account)
+	dictionary := dict.Dictionary{"first": "First word"}
+	//dictionary["hello"] = "hello"
+	//fmt.Println(dictionary)
+	//fmt.Println(dictionary["first"])
+	/*
+		definition, err := dictionary.Search("first")
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(definition)
+		}
+	*/
+	/*
+		word := "hello"
+		definition = "Greeting"
+		err = dictionary.Add(word, definition)
+		if err != nil {
+			fmt.Println(err)
+		}
+		hello, _ := dictionary.Search(word)
+		fmt.Println("found", word, "definition:", hello)
+		err2 := dictionary.Add(word, definition)
+		if err2 != nil {
+			fmt.Println(err2)
+		}
+	*/
+	/*
+		baseWord := "hello"
+		dictionary.Add(baseWord, "First")
+		err := dictionary.Update(baseWord, "Second")
+		if err != nil {
+			fmt.Println(err)
+		}
+		word, _ := dictionary.Search(baseWord)
+		fmt.Println(word)
+	*/
 
-	account := accounts.NewAccount("heekng")
-	fmt.Println(account)
-	account.Deposit(10)
-	fmt.Println(account.Balance())
-	//err := account.Withdraw(20)
-	//if err != nil {
-	//log.Fatalln(err)
-	//fmt.Println(err)
-	//}
-	fmt.Println(account.Balance(), account.Owner())
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(word)
+	}
 }
