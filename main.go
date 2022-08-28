6ed672bdd586ccdc366ba0b77b5b1977f58eb3c4
heekng <rhkd1769@gmail.com>
2022-08-28 14:04:45 +0900

    pointer

--- main.go
+++ main.go
@@ -2,35 +2,19 @@ package main
 
 import "fmt"
 
-func canIDrink(age int) bool {
-	//switch age {
-	//case 10:
-	//	return false
-	//case 18:
-	//	return true
-	//}
-	//return false
-
-	//switch {
-	//case age < 19:
-	//	return false
-	//case age == 18:
-	//	return true
-	//case age > 50:
-	//	return false
-	//
-	//}
-	//return false
-
-	switch koreanAge := age + 2; koreanAge {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-	return false
-}
-
 func main() {
-	fmt.Println(canIDrink(16))
+	//a := 2
+	//b := a
+	//a = 10
+	//fmt.Println(a, b)
+	// 주소값을 확인
+	//fmt.Println(&a, &b)
+
+	a := 2
+	b := &a
+	a = 5
+	// *: 살펴본다, 훑어본다., 메모리에 할당된 값을 확인한다.
+	*b = 20
+	fmt.Println(*b)
+	fmt.Println(a)
 }
