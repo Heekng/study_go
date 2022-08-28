f1be8a755e3fd4f26cabea6f9da5f53d7e54b74e
heekng <rhkd1769@gmail.com>
2022-08-28 13:31:21 +0900

    함수 2

--- main.go
+++ main.go
@@ -5,29 +5,17 @@ import (
 	"strings"
 )
 
-func multiply(a, b int) int {
-	return a * b
-}
+// naked return
+func lenAndUpper(name string) (length int, uppercase string) {
+	// defer: funtion이 끝난 후 작동
+	defer fmt.Println("I'm done")
 
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
-	//fmt.Println(multiply(2, 2))
-
-	// go 는 한번도 사용하지 않는 변수, 상수에 대해 에러를 나타낸다.
-	//totalLength, upperName := lenAndUpper("heekng")
-	//fmt.Println(totalLength, upperName)
-
-	//아래와 같이 리턴값의 개수를 조절할 수 있다.
-	//totalLength, _ := lenAndUpper("heekng")
-	//fmt.Println(totalLength)
-
-	// 다중
-	repeatMe("h", "e", "e", "k", "n", "g")
+	totalLength, up := lenAndUpper("heekng")
+	fmt.Println(totalLength, up)
 }
