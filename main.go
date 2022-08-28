32ccb2423b5be9b86ab5c6139d8aab1ee9736e73
heekng <rhkd1769@gmail.com>
2022-08-28 17:44:10 +0900

    Channels 2

--- main.go
+++ main.go
@@ -6,9 +6,22 @@ import (
 )
 
 func main() {
-	go coolCount("heekng")
-	go coolCount("test")
-	time.Sleep(time.Second * 5)
+	c := make(chan string)
+	people := [4]string{"hee", "kng", "test1", "test2"}
+	for _, person := range people {
+		go isCool(person, c)
+	}
+	fmt.Println("Waiting for messages")
+	//resultOne := <-c
+	//resultTwo := <-c
+	//resultThree := <-c
+	for i := 0; i < len(people); i++ {
+		fmt.Print("waiting for", i)
+		fmt.Println(<-c)
+	}
+	//fmt.Println("Received this message: " + resultOne)
+	//fmt.Println("Received this message: " + resultTwo)
+	//fmt.Println("Received this message: " + resultThree)
 }
 
 func coolCount(person string) {
@@ -16,4 +29,9 @@ func coolCount(person string) {
 		fmt.Println(person, "is cool", i)
 		time.Sleep(time.Second)
 	}
+}
+
+func isCool(person string, c chan string) {
+	time.Sleep(time.Second * 10)
+	c <- person + " is cool"
 }
